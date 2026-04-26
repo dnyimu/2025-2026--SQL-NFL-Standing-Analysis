e3a23251b147ecc69caf1324216fea67f886de33
dnyimu <dnyimu98@gmail.com>
2026-04-26 13:47:25 -0400

    Update NFC Most Wins Ranking.sql

--- NFC Most Wins Ranking.sql
+++ NFC Most Wins Ranking.sql
@@ -10,6 +10,19 @@ Group By
 ORDER By
 	Wins desc;
 
+-- Top 5 most wins in NFC
+    SELECT
+	Team,
+    Wins,
+  DENSE_RANK() OVER( order by Wins desc)  AS WinsRanking  
+From
+	nfc_2025_2026_standings
+Group By
+	Team, Wins
+ORDER By
+	Wins desc
+    limit 5
+
 -- Most wins in NFC North Ranked
 SELECT
 	Team,
