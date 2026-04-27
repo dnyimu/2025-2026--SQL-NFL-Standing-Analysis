7ae597559034d27bf24acf4ab98e302225786a9f
dnyimu <dnyimu98@gmail.com>
2026-04-26 21:37:52 -0400

    Add files via upload

---
+++ NFC Defensive Efficiency Case Statements.sql
@@ -0,0 +1,23 @@
+---  NFC Defensive Efficiency  Case statement
+--  Below 0.0 score= below avg team
+-- 0.0 score= avg team
+-- Above 0.0 score= Above avg team
+
+SELECT
+	Team,
+	Defensive_Efficiency_Score,
+CASE
+WHEN 
+	 Defensive_Efficiency_Score < 0.0 then "Inefficient Defense"
+WHEN 
+	Defensive_Efficiency_Score = 0.0 then "Average Defensive Efficiency"
+ELSE 
+	"Efficient Defense"
+END AS 
+	"Defensive Team Rating"
+FROM
+	nfc_2025_2026_standings
+GROUP BY
+	Team, Defensive_Efficiency_Score
+ORDER BY
+	 Defensive_Efficiency_Score asc;
---
+++ NFC Offensive Efficiency Case Statements.sql
@@ -0,0 +1,23 @@
+---  NFC Offensive Efficiency offense Case statement
+--  Below 0.0 score= below avg team
+-- 0.0 score= avg team
+-- Above 0.0 score= Above avg team
+
+SELECT
+	team,
+    Offensive_Efficiency_Score,
+CASE
+WHEN 
+	 Offensive_Efficiency_Score < 0.0 then "Inefficient Offense"
+WHEN 
+	Offensive_Efficiency_Score = 0.0 then "Average Offensive Efficiency"
+ELSE 
+	"Efficient Offense"
+END AS 
+	"Team Rating"
+FROM
+	nfc_2025_2026_standings
+GROUP BY
+	Team, Offensive_Efficiency_Score
+ORDER BY
+	 Offensive_Efficiency_Score asc;
