5ff0447c445bd1401b7e229bd8aec6061b7e388c
dnyimu <dnyimu98@gmail.com>
2026-04-27 22:25:05 -0400

    Add files via upload

---
+++ NFC Strength of Schedule Rank.sql
@@ -0,0 +1,11 @@
+-- Team that had the hardest schedule( higher the number, the harder their schedule was)
+SELECT
+	Team,
+	Strength_of_schedule,
+	DENSE_RANK () OVER( order by strength_of_schedule desc) As ScheduleDifficultyRank
+FROM
+	nfc_2025_2026_standings
+GROUP BY
+	team, Strength_Of_Schedule
+ORDER BY
+	Strength_Of_Schedule desc
